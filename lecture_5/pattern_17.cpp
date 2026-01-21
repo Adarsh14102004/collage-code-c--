c30aca6460c11437ab04d1d8a9f8c0fc08234cbb
Adarsh14102004 <kalmodiyaadarsh@2004@gmail.com>
2026-01-21 14:42:03 +0530

    pattern19

--- lecture_5/pattern_17.cpp
+++ lecture_5/pattern_17.cpp
@@ -1,8 +1,4 @@
-// *               *
-// * *           * *
-// * * *       * * *
-// * * * * * * * * * 
-#include <iostream>
+ #include <iostream>
 using namespace std;
 
 int main() {
@@ -21,7 +17,7 @@ int main() {
         for (int j = 1; j <= i; j++)
         {
             cout<<"* ";
-        }
+        };
         cout<<endl;
     }
     return 0;
---
+++ lecture_5/pattern_19.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    // Upper part
+    for(int i = n; i >= 1; i--) {
+        for(int j = 1; j <= (n - i) * 4; j++) {
+            cout << " ";
+        }
+        for(int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    // Lower part
+    for(int i = 2; i <= n; i++) {
+        for(int j = 1; j <= (n - i) * 4; j++) {
+            cout << " ";
+        }
+        for(int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
