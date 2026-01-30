b5ddc10886ae30ee53578754bfa9ba696d7cdb2d
Adarsh14102004 <kalmodiyaadarsh@2004@gmail.com>
2026-01-30 22:01:38 +0530

    practice1

--- practice question/practice_1.cpp
+++ practice question/practice_1.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main() {
     int n;
-    cin >> n;   // size of pattern (odd number)
+    cin >> n;   // n should be an odd number, e.g. 5
 
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j < n; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
 
-            // Border condition
+            // Border stars
             if (i == 0 || i == n-1 || j == 0 || j == n-1) {
                 cout << "* ";
             }
-            // Center condition
+            // Center symbol
             else if (i == n/2 && j == n/2) {
                 cout << "@ ";
             }
