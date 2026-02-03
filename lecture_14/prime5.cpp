cfcbb99f54ca67e8910fc17db02963ac80adc4f3
Adarsh14102004 <kalmodiyaadarsh@2004@gmail.com>
2026-02-03 16:16:17 +0530

    prime5

---
+++ lecture_14/prime5.cpp
@@ -0,0 +1,15 @@
+// sum of digit and negative 
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int sum = 0;
+    while (n != 0) {
+        int digit = n % 10;
+        sum = sum + digit;
+        n = n / 10;
+    }
+    cout << "Sum of digits: " << sum << endl;
+    return 0;
+}
