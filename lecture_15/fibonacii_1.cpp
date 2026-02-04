bef5c9e7dc6bd0c267f87360359c1c2a94dea2cf
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-02-04 21:27:57 +0530

    fibonacii 1

--- lecture_15/fibonacii_1.cpp
+++ lecture_15/fibonacii_1.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
-int fibbo(int n) {
+int fibo(int n) {
     if(n == 0 || n == 1) {
         return n;
     }
     // recursion
-    return fibbo(n-1) + fibbo(n-2);
+    return fibo(n-1) + fibo(n-2);
 }
 int main() {
     int n;
     cin >> n;
-    cout << fibbo(n) << endl;
+    cout << fibo(n) << endl;
     return 0;
 }
