56e598b5708c34b91ad508f476960c4eb4bfe91f
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-02-04 21:26:47 +0530

    fibonaci series

--- lecture_15/fibbo_2.cpp
+++ lecture_15/fibbo_2.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 using namespace std;
-int fibbo(int n) {
+int fibo(int n) {
     if(n == 0 || n == 1) {
         return n;
     }
     // recursion
-    return fibbo(n-1) + fibbo(n-2);
+    return fibo(n-1) + fibo(n-2);
 }
 int main() {
     int n;
     cin >> n;
     for (int i = 0; i < n-1; i++)
     {
-        cout<<fibbo(i)<<" ";    
+        cout<<fibo(i)<<" ";    
     }
     
     return 0;
