187c88b0792e97cb6f8b02bc0d0bc514b5117faa
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-02-05 20:46:11 +0530

    pattern19

--- lecture_5/pattern_19.cpp
+++ lecture_5/pattern_19.cpp
@@ -6,7 +6,7 @@ int main() {
     cin>>n;
 
     
-    for(int i = n; i >= 1; i--) {
+    for(int i = n; i>=1; i--) {
         for(int j = 1; j <= (n - i) * 4; j++) {
             cout << " ";
         }
