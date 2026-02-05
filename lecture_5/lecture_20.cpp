cd0241782bac138e76b59ac6ae9741868c645228
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-02-05 19:48:19 +0530

    pattern20

--- lecture_5/lecture_20.cpp
+++ lecture_5/lecture_20.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int m;
-    cin>>m;
-    int n = (m+1)/2;
+    int M;
+    cin>>M;
+    int n = (M+1)/2;
     for(int i=1; i<=n; i++) {
         for(int j=1; j<=n-i; j++) {
             cout<<"  ";
