4e22b0b15cd8fce0d408e2bfce72411c81cdddf7
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-02-06 14:09:02 +0530

    swastik1

---
+++ practice question/swastik.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+int main() {
+int i = 1;
+int n ;
+cin>>n;
+if(n>=5)
+{
+int mid =  n/2+1;
+while(i<=n)
+{
+    int j = 1;
+    while (j<=n)
+    {
+        if (i==mid)
+       {cout<<" *"; 
+        }
+       else if( i==1 && j==mid )
+        {     
+            int k = mid;
+            while(k<=n)
+            {
+                cout<<" *";
+                k++;
+            }
+
+        } 
+       else if(i<=mid && j==1||j==mid)
+       {
+        cout<<" *";
+       }
+        else if(i>=mid && j==n)
+        {cout<<" *";
+        }
+        
+        else if(i==n&&j<=mid){
+            cout<<" *";
+        }
+       else{
+        cout<<"  ";
+           }
+       j++;
+    }
+
+cout<<endl;
+i++;
+}
+}
+else cout<<"Wrong input";
+}
